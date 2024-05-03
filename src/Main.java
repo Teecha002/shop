7b8b1b9c56d7fbdfac4c239d7b0adde2400d288e
Teech <Teecha003@gmail.com>
2024-05-03 15:43:37 +0700

    Final shop

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,62 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int Menu1 = 0;
+        int Menu2 = 0;
+        int Menu3 = 0;
+        int Menu4 = 0;
+        int Menu5 = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("สวัสดีครับคุณลูกค้าไม่ทราบว่าคุณลูกค้าต้องการรับประทานอะไรดีครับ");
+        System.out.println("1.ส้มตำปูปาร้า");
+        System.out.println("2.ตำข้าวโพดเเซ่บนัวเวอร์");
+        System.out.println("3.ไก่ย่างพันดาว");
+        System.out.println("4.ลูกชิ้นเนื้อหมูสวรรค์");
+        System.out.println("5.ก๋วยเตี๋ยวต้มยำน้ำใสเครื่องข้น");
+        System.out.println("กด 6 เพื่อยกเลิกการสั่งหรือสรุปรายการ");
+
+        int choiceMenu;
+        do {
+            System.out.println("กรุณาเลือกหมายเลขเมนูที่ต้องการ (หรือกด 6 เพื่อยกเลิกรายการ): ");
+            choiceMenu = scanner.nextInt();
+            switch (choiceMenu) {
+                case 1:
+                    System.out.println("คุณเลือก: ส้มตำปูปาร้า");
+                    Menu1++;
+                    break;
+                case 2:
+                    System.out.println("คุณเลือก: ตำข้าวโพดแซ่บนัวเวอร์");
+                    Menu2++;
+                    break;
+                case 3:
+                    System.out.println("คุณเลือก: ไก่ย่างพันดาว");
+                    Menu3++;
+                    break;
+                case 4:
+                    System.out.println("คุณเลือก: ลูกชิ้นเนื้อหมูสวรรค์");
+                    Menu4++;
+                    break;
+                case 5:
+                    System.out.println("คุณเลือก: ก๋วยเตี๋ยวต้มยำน้ำใสเครื่องข้น");
+                    Menu5++;
+                    break;
+                case 6:
+                    System.out.println("ยกเลิกรายการ");
+                    break;
+                default:
+                    System.out.println("กรุณาเลือกหมายเลขเมนูที่ถูกต้อง");
+            }
+
+        } while (choiceMenu != 6);
+
+        System.out.println("สรุปจำนวนที่สั่ง:");
+        System.out.println("1. ส้มตำปูปาร้า: " + Menu1);
+        System.out.println("2. ตำข้าวโพดแซ่บนัวเวอร์: " + Menu2);
+        System.out.println("3. ไก่ย่างพันดาว: " + Menu3);
+        System.out.println("4. ลูกชิ้นเนื้อหมูสวรรค์: " + Menu4);
+        System.out.println("5. ก๋วยเตี๋ยวต้มยำน้ำใสเครื่องข้น: " + Menu5);
+
         }
     }
-}
